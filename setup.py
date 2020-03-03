08b7c56d0a8230efae804e21a4142b8f3f57c8b0
p3r7 <11557146+p3r7@users.noreply.github.com>
2020-03-03 22:32:46 +0100

    New release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ai-dungeon-cli",
-    version="0.1.2",
+    version="0.1.3",
     author="Jordan Besly",
     author_email="",
     description="Play ai dungeon from your terminal",
